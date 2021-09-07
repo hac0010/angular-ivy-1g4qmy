38016ec98f778cb010940c292026d336d6a11116
cwhitlatch77 <cjw0061@mix.wvu.edu>
2021-09-07 15:44:48 -0400

    Initial commit

---
+++ src/app/headers/top-bar.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-top-bar',
+  templateUrl: './top-bar.component.html',
+  styles: [`h1 { font-family: Arial; }`]
+})
+
+export class TopBarComponent {}
